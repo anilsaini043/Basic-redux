51cc963d87666109f672bde5b6634d3156bcdd3c
anilsaini <asaini@hgsdigital.com>
2021-10-23 14:38:45 +0530

    Redux app cleanup

--- src/App.js
+++ src/App.js
@@ -1,56 +1,22 @@
 import React from 'react';
 import './App.css';
-import Child from "./childComponent";
-import { nameAction, foodAction } from "./redux/actions/actions"
-import { connect } from "react-redux";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {brand: "TATA", counter:''};
+    this.state = {
+      title: "Redux with Thunk"
+    };
   }
 
-  changeBrand(count){
-    this.setState({counter:count})
-  }
-  
-  render(props) {
+  render() {
     return (
       <div className="main-container">
-        <h2>React with Redux</h2>
-        <div className="container">
-          <div>
-            <p>Data From Redux Store</p>
-            <hr />
-            <p>Change Name: {this.props.myName} </p>
-            <p>Change Food: {this.props.myFood} </p>
-            <button onClick={() => { this.props.changeMyName("Deepu") }}>Change Name</button>
-            <button onClick={() => { this.props.changeMyFood("Panner") }}>Change Food</button>
-          </div>
-          <div style={{ marginLeft: '20px' }}>
-            <p>Data coming from Child and Parent</p>
-            <hr />
-            <p>From Child: {this.state.counter} </p>
-            <Child brand={this.state.brand} changeBrand={this.changeBrand.bind(this)}/>
-          </div>
-        </div>
+        <h2>{this.state.title}</h2>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    myName: state.changeName.name,
-    myFood: state.changeFood.food
-  }
-}
 
-const mapDispatchToProps = (dispatch) => { 
-  return {
-    changeMyName: (name) => { dispatch(nameAction(name)) },
-    changeMyFood: (food) => { dispatch(foodAction(food)) },
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import rootReducer from './redux/reducers';
-
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
